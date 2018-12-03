37cb39bedad5d8673609e20c106fa7d11ae5d1e3
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2018-12-03 23:45:04 +0800

    Add files via upload

---
+++ pget.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+
+helpmsg ='''------MCD pget插件------
+!!pget [URL] -下载这个插件
+--------------------------------'''
+
+def onServerInfo(server, info):
+  if info.isPlayer == 1:
+    if info.content.startswith('!!pget'):
+      args = info.content.split(' ')
+      if (len(args) == 1):
+        for line in helpmsg.splitlines():
+          server.tell(info.player, line)
+      elif (len(args) == 2):
+        result = os.system('cd plugins && wget ' + args[1])
+        if result == 0:
+          server.say('scucess')
+        else:
+          server.say('failed to download')
