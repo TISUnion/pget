fb0d85a22b51e6e6ca662f0a4e021b1216b3f4db
chino_desu <33673786+kafuuchino-desu@users.noreply.github.com>
2019-02-27 19:58:26 +0800

    add whitelist

--- pget.py
+++ pget.py
@@ -8,13 +8,18 @@ helpmsg ='''------MCD pget插件------
 def onServerInfo(server, info):
   if info.isPlayer == 1:
     if info.content.startswith('!!pget'):
-      args = info.content.split(' ')
-      if (len(args) == 1):
-        for line in helpmsg.splitlines():
-          server.tell(info.player, line)
-      elif (len(args) == 2):
-        result = os.system('cd plugins && wget -N ' + args[1])
-        if result == 0:
-          server.say('success')
-        else:
-          server.say('failed to download')
+      with open('./plugins/pget/whitelist.json') as handle:
+        whitelist = handle.readlines()
+      if  info.player in whitelist:
+        args = info.content.split(' ')
+        if (len(args) == 1):
+          for line in helpmsg.splitlines():
+            server.tell(info.player, line)
+        elif (len(args) == 2):
+          result = os.system('cd plugins && wget -N ' + args[1])
+          if result == 0:
+            server.say('success')
+          else:
+            server.say('failed to download')
+      else:
+        server.say('Permission denied')
